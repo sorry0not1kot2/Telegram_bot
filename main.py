a13eaf976981ac4951bafb4359c6281524062979
sorry0not1kot2 <162019473+sorry0not1kot2@users.noreply.github.com>
2024-06-30 15:29:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Настройка бота
-BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')  # Используем переменную окружения для токена
-bot = AsyncTeleBot(BOT_TOKEN)
+bot = AsyncTeleBot(os.getenv('TELEGRAM_BOT_TOKEN'))
 
 async def get_gpt_response(query):
     try:
         response = await g4f.ChatCompletion.create_async(
-            model="gpt-4",
+            model="gpt-4o",
             messages=[{"role": "user", "content": query}],
         )
         return response
