9449c8f97565dd5e751a1081e51d34b1280f93f9
sorry0not1kot2 <162019473+sorry0not1kot2@users.noreply.github.com>
2024-07-10 00:49:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,32 +2,36 @@
 #
 #
 # файл mmain.py
-
 import g4f
 import logging
 
 # Настройка логирования
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format='%(asctime)s - %(name)s - %(levellevel)s - %(message)s',
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
 def list_available_models_and_providers():
     try:
-        # Получение списка доступных моделей
-        available_models = g4f.models.list_models()
-        logger.info(f"Доступные модели: {available_models}")
-
         # Получение списка доступных провайдеров
-        available_providers = g4f.providers.list_providers()
-        logger.info(f"Доступные провайдеры: {available_providers}")
+        available_providers = g4f.Provider.__subclasses__()
+        logger.info("Доступные провайдеры:")
+        for provider in available_providers:
+            logger.info(provider.__name__)
+
+        # Получение списка доступных моделей для каждого провайдера
+        for provider in available_providers:
+            provider_instance = provider()
+            available_models = provider_instance.models
+            logger.info(f"Доступные модели для провайдера {provider.__name__}: {available_models}")
 
     except Exception as e:
         logger.error(f"Error listing models and providers: {e}")
 
 if __name__ == '__main__':
     list_available_models_and_providers()
+
 
 
 
